78e027f4a715684ecb2014af540d8200156c1851
alexeygolovnev <golovnevalexey@gmail.com>
2023-05-02 20:11:46 +0300

    alert

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -3,6 +3,7 @@ import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import axios from "axios";
 import { Buffer } from "buffer";
+import { Alert, Snackbar } from "@mui/material";
 
 type CSVFileImportProps = {
   url: string;
@@ -11,7 +12,7 @@ type CSVFileImportProps = {
 
 export default function CSVFileImport({ url, title }: CSVFileImportProps) {
   const [file, setFile] = React.useState<File | null>();
-
+  const [error, setError] = React.useState<{ message: string }>(null);
   const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files;
     if (files && files.length > 0) {
@@ -34,27 +35,30 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
         );
         localStorage.setItem("authToken", token);
       }
+      try {
+        const response = await axios({
+          method: "GET",
+          url,
+          params: {
+            name: encodeURIComponent(file.name),
+          },
+          headers: {
+            Authorization: `Basic ${token}`,
+          },
+        });
+        console.log("File to upload: ", file.name);
+        console.log("Uploading to: ", response.data);
 
-      const response = await axios({
-        method: "GET",
-        url,
-        params: {
-          name: encodeURIComponent(file.name),
-        },
-        headers: {
-          Authorization: `Basic ${token}`,
-        },
-      });
-      console.log("File to upload: ", file.name);
-      console.log("Uploading to: ", response.data);
+        const result = await axios.put(response.data.url, file, {
+          headers: {
+            "Content-Type": file.type,
+          },
+        });
+        console.log("Result: ", result);
+      } catch (e: { message: string }) {
+        setError({ message: e.message });
+      }
 
-      const result = await axios.put(response.data.url, file, {
-        headers: {
-          "Content-Type": file.type,
-        },
-      });
-
-      console.log("Result: ", result);
       setFile(null);
     }
   };
@@ -71,6 +75,15 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
           <button onClick={uploadFile}>Upload file</button>
         </div>
       )}
+      <Snackbar
+        anchorOrigin={{ horizontal: "right", vertical: "top" }}
+        open={!!error}
+        autoHideDuration={3000}
+      >
+        <Alert severity="error" sx={{ width: "100%" }}>
+          {error?.message}
+        </Alert>
+      </Snackbar>
     </Box>
   );
 }
